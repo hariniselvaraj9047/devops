ee12809bdead164a41567d416c64098ff4895e6d
hariniselvaraj9047 <harinisms03@gmail.com>
2025-07-14 15:41:15 +0530

    Update Policy.tf

--- Policy.tf
+++ Policy.tf
@@ -1,3 +1,3 @@
-resourc "aws_iam_user_policy_attachment" "cloud" {
-user = [aws_iam_user.cloud.name}
+resourc "aws_iam_user_policy_attachment" "harini@9047" {
+user = [aws_iam_user.harini@9047.name}
 policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
